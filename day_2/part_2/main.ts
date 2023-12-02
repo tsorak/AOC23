57f461c54b087861918332baf2a7769044668c0a
Karl Osterberg <karost0249@gmail.com>
2023-12-02 12:10:41 +0100

    day 2 part 2

--- day_2/part_2/main.ts
+++ day_2/part_2/main.ts
@@ -6,12 +6,10 @@ function main() {
     const [game_id] = l.split(":");
     const game = l.split(":").splice(1).join().trim();
 
-    const too_big = [false, false, false];
-
-    game.split(";").forEach((set) => {
+    const too_big = game.split(";").map((set) => {
       set = set.trim();
 
-      const big_in_set = [false, false, false];
+      const big_in_set = [1, 1, 1];
 
       set.split(",").forEach((hand) => {
         hand = hand.trim();
@@ -20,33 +18,29 @@ function main() {
         const score = Number(string_score);
         const color = unknown_color as "red" | "green" | "blue";
 
-        if (color == "red" && score > 12) {
-          big_in_set[0] ||= true;
-        } else if (color == "green" && score > 13) {
-          big_in_set[1] ||= true;
-        } else if (color == "blue" && score > 14) {
-          big_in_set[2] ||= true;
+        if (color == "red") {
+          big_in_set[0] = score > big_in_set[0] ? score : big_in_set[0];
+        } else if (color == "green") {
+          big_in_set[1] = score > big_in_set[1] ? score : big_in_set[1];
+        } else if (color == "blue") {
+          big_in_set[2] = score > big_in_set[2] ? score : big_in_set[2];
         }
       });
 
-      big_in_set.forEach((c, i) => {
-        too_big[i] ||= c;
-      });
+      return big_in_set;
+    }).reduce((acc, curr) => {
+      const bigger_replaces_smaller = [];
+      bigger_replaces_smaller[0] = curr[0] > acc[0] ? curr[0] : acc[0];
+      bigger_replaces_smaller[1] = curr[1] > acc[1] ? curr[1] : acc[1];
+      bigger_replaces_smaller[2] = curr[2] > acc[2] ? curr[2] : acc[2];
+      return bigger_replaces_smaller;
     });
 
-    return [game_id, too_big] as [string, [boolean, boolean, boolean]];
+    return [game_id, too_big] as [string, [number, number, number]];
   });
 
   const won_games = games.map(
-    ([game_id_str, too_big]) => {
-      const [red, green, blue] = too_big;
-      if (red || green || blue) {
-        return 0;
-      } else {
-        const game_id = Number(game_id_str.split(" ").at(-1));
-        return game_id;
-      }
-    },
+    ([_, [red, green, blue]]) => red * green * blue,
   ).reduce((acc, v) => acc + v);
 
   console.log(won_games);
